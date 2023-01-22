702e17b39cf7e69b5b86edb5482b44693f346d38
Satwikarachamsetty <95457443+Satwikarachamsetty@users.noreply.github.com>
2023-01-22 22:07:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function temp()
+{
+let type=document.getElementById("Typeofdegree").value;
+let degree=document.getElementById("degrees").value;
+event.preventDefault();
+var result=0;
+if(type=="celsius")
+{
+result=(1.8*degree)+32;
+document.getElementById('result').value=result+'\u00B0 F';
+}
+else{
+    result=(degree-32)/1.8;
+    document.getElementById('result').value=result+'\u00B0 C';
+}
+
+}
